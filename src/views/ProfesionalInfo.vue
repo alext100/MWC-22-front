e927321b92fc0d4f818a1118ad403c663765b823
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-12 03:53:14 +0100

    View ProfesionalInfo created

---
+++ src/views/ProfesionalInfo.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container"></div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProfesionalInfo",
+});
+</script>
