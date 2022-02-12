5f8af8f08fa976a300ea3efba24d6fbef839b88d
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-12 13:57:20 +0100

    Test for registerUser action added

--- tests/unit/actions.spec.js
+++ tests/unit/actions.spec.js
@@ -10,3 +10,23 @@ describe("When the action deleteDataFromLocalStorage is invoked", () => {
     expect(commit).toHaveBeenCalledWith("logoutUser");
   });
 });
+
+describe("When the action registerUser is invoked", () => {
+  test("Then it should invoke commit with setCurrentUserRegisterData and received data", async () => {
+    const userData = {
+      firstName: "Alex",
+      lastName: "Alex",
+      email: "alex@email.com",
+      country: "España",
+      city: "Barcelona",
+      description: "Description",
+    };
+    jest.fn().mockResolvedValue({
+      userData,
+    });
+
+    await actions.registerUser(configActionContext(commit), userData);
+
+    expect(commit).toHaveBeenCalledWith("setCurrentUserRegisterData", userData);
+  });
+});
