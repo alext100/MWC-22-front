ea5d0b8c71e2f8cf35e7295a6a6a926903e9bfba
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-02-13 14:13:52 +0100

    Vue-next-select installed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import BootstrapVue3 from "bootstrap-vue-3";
+import VueNextSelect from "vue-next-select";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "vue-next-select/dist/index.min.css";
 
+createApp({}).component("vue-select", VueNextSelect);
 createApp(App).use(store).use(router).use(BootstrapVue3).mount("#app");
